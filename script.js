e4ccd9d23df450007ecc1b43932397f915993f79
DarielRafa <gamingdariel1@gmail.com>
2025-09-08 11:11:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function() {
+  $("#kota").select2({
+    placeholder: "Ketik atau pilih kota",
+    allowClear: true
+  });
+});
+
+
+function setBackgroundByTime() {
+  let jam = new Date().getHours();
+  let body = document.body;
+
+  if (jam >= 0 && jam < 10) {
+    body.className = "pagi";   
+  } else if (jam >= 10 && jam < 11) {
+    body.className = "siang";  
+  } else {
+    body.className = "malam";  
+  }
+
+  console.log("Sekarang jam: " , jam,  "| class body: ", body.className);
+}
+
+
+setBackgroundByTime();
+
+
+setInterval(setBackgroundByTime, 60000);
